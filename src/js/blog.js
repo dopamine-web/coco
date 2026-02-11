18e7674445d4051f1227d3be352d76d00bf8e10d
Anna <annadrozalina@mail.ru>
2026-02-11 12:41:05 +0200

    created section hero and believe

---
+++ src/js/blog.js
@@ -0,0 +1,10 @@
+import '../scss/blog.scss';
+
+// components
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+import { useBelieveSlider } from './components/blog/slider';
+
+useTheme();
+useBurger();
+useBelieveSlider();
